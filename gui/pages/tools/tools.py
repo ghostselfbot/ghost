41599621ba86b39d0df25073169c587e8468e0a5
Benny <83777519+bennyscripts@users.noreply.github.com>
2026-02-15 20:45:42 +0000

    Change tools card colours

--- gui/pages/tools/tools.py
+++ gui/pages/tools/tools.py
@@ -14,7 +14,7 @@ class ToolsPage:
         self.images = images
         self.layout = layout
         self.position_resize_grips = position_resize_grips
-        self.hover_colour = Style.TOOL_HOVER.value
+        self.hover_colour = self.root.style.colors.get("secondary")
         
         self.surveillance_page = SurveillancePage(self, root, bot_controller, images, layout)
         self.message_logger_page = MessageLoggerPage(self, root, bot_controller, images, layout)
@@ -84,30 +84,30 @@ class ToolsPage:
         widget.bind("<Leave>", on_leave)
         
     def _draw_page_card(self, parent, page):
-        page_wrapper = RoundedFrame(parent, radius=15, bootstyle="secondary.TFrame")
+        page_wrapper = RoundedFrame(parent, radius=15, bootstyle="dark.TFrame")
         page_wrapper.bind("<Button-1>", lambda e, cmd=page["command"]: cmd())
 
         page_title = ttk.Label(page_wrapper, text=page["name"], font=("Host Grotesk", 14 if sys.platform != "darwin" else 18, "bold"), justify=ttk.CENTER)
-        page_title.configure(background=self.root.style.colors.get("secondary"))
+        page_title.configure(background=self.root.style.colors.get("dark"))
         page_title.grid(row=0, column=0, pady=(25, 5))
         page_title.bind("<Button-1>", lambda e, cmd=page["command"]: cmd())
 
         page_description = ttk.Label(page_wrapper, text=page["description"], wraplength=175, justify=ttk.CENTER)
-        page_description.configure(background=self.root.style.colors.get("secondary"), foreground=Style.LIGHT_GREY.value)
+        page_description.configure(background=self.root.style.colors.get("dark"), foreground=Style.LIGHT_GREY.value)
         page_description.grid(row=1, column=0, pady=(0, 25))
         page_description.bind("<Button-1>", lambda e, cmd=page["command"]: cmd())
         
         # page_icon = ttk.Label(page_wrapper, image=self.images.get("right-chevron"))
-        # page_icon.configure(background=self.root.style.colors.get("secondary"))
+        # page_icon.configure(background=self.root.style.colors.get("dark"))
         # page_icon.grid(row=0, column=1, sticky=ttk.E, padx=(0, 20), pady=15)
         
         page_wrapper.grid_columnconfigure(0, weight=1)
         page_wrapper.grid_rowconfigure(0, weight=1)
         page_wrapper.grid_rowconfigure(1, weight=1)
-        self._bind_hover_effects(page_wrapper, [page_title, page_wrapper, page_description], self.hover_colour, self.root.style.colors.get("secondary"))
-        self._bind_hover_effects(page_title, [page_title, page_wrapper, page_description], self.hover_colour, self.root.style.colors.get("secondary"))
-        self._bind_hover_effects(page_description, [page_title, page_wrapper, page_description], self.hover_colour, self.root.style.colors.get("secondary"))
-        # self._bind_hover_effects(page_icon, [page_title, page_wrapper, page_icon], self.hover_colour, self.root.style.colors.get("secondary"))
+        self._bind_hover_effects(page_wrapper, [page_title, page_wrapper, page_description], self.hover_colour, self.root.style.colors.get("dark"))
+        self._bind_hover_effects(page_title, [page_title, page_wrapper, page_description], self.hover_colour, self.root.style.colors.get("dark"))
+        self._bind_hover_effects(page_description, [page_title, page_wrapper, page_description], self.hover_colour, self.root.style.colors.get("dark"))
+        # self._bind_hover_effects(page_icon, [page_title, page_wrapper, page_icon], self.hover_colour, self.root.style.colors.get("dark"))
         
         return page_wrapper
         
@@ -124,7 +124,7 @@ class ToolsPage:
         row, col = 1, 0
         for page in self.pages:
             card = self._draw_page_card(parent, page)
-            card.grid(row=row, column=col, sticky=ttk.NSEW, padx=5, pady=5)
+            card.grid(row=row, column=col, sticky=ttk.NSEW, padx=(0, 5) if col == 0 else (5, 0), pady=5)
             col += 1
             if col > 1:
                 col = 0
