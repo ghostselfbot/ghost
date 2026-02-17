c5f2f838d506756095c90908d5a944e44cf52ba3
Benny <83777519+bennyscripts@users.noreply.github.com>
2026-02-17 22:07:09 +0000

    Fix merge conflicts

--- gui/main.py
+++ gui/main.py
@@ -43,23 +43,14 @@ class GhostGUI:
         self.root.style = ttk.Style()
         # self.root.style.theme_use("darkly")
         self.root.style.load_user_themes(resource_path("data/gui_theme.json"))
-<<<<<<< HEAD
         
         apply_theme(self.root, self.cfg.get("gui_theme") if self.cfg.get("gui_theme") else "ghost")
         
-        self.root.style.configure("TEntry",       background=self.root.style.colors.get("dark"), fieldbackground=Style.ENTRY_BG.value, font=("Host Grotesk",), bordercolor=Style.ENTRY_BG.value, foreground=Style.ENTRY_FG.value, borderstyle="flat", borderwidth=0)
-        self.root.style.configure("TCheckbutton", background=self.root.style.colors.get("dark"), font=("Host Grotesk",))
-        self.root.style.configure("TMenubutton",  font=("Host Grotesk",))
-        self.root.style.configure("TLabel",       font=("Host Grotesk",))
-        self.root.style.configure("TButton",      font=("Host Grotesk",))
-=======
-        self.root.style.theme_use("ghost")
         self.root.style.configure("TEntry",       background=self.root.style.colors.get("dark"), fieldbackground=Style.ENTRY_BG.value, font=("Host Grotesk", 12 if sys.platform != "darwin" else 13), bordercolor=Style.ENTRY_BG.value, foreground="#ffffff", borderstyle="flat", borderwidth=0)
         self.root.style.configure("TCheckbutton", background=self.root.style.colors.get("dark"), font=("Host Grotesk", 12 if sys.platform != "darwin" else 13))
         self.root.style.configure("TMenubutton",  font=("Host Grotesk", 12 if sys.platform != "darwin" else 13))
         self.root.style.configure("TLabel",       font=("Host Grotesk", 12 if sys.platform != "darwin" else 13))
         self.root.style.configure("TButton",      font=("Host Grotesk", 12 if sys.platform != "darwin" else 13))
->>>>>>> origin/main
         
         if sys.platform == "darwin":
             self.root.attributes("-transparent", True)
