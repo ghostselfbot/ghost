3fd078118b0d58eed48be729acf88b5006e3f660
Benny <83777519+bennyscripts@users.noreply.github.com>
2026-07-29 12:51:47 +0100

    Change version back to 4.2.3-dev

--- utils/config/__init__.py
+++ utils/config/__init__.py
@@ -4,7 +4,7 @@ from .theme import Theme
 from .config import Config
 from .token import Token
 
-VERSION = "4.2.1"
+VERSION = "4.2.3-dev"
 PRODUCTION = False
 MOTD = "lil hotfix cuz i forgor bugs"
 CHANGELOG = """"""
