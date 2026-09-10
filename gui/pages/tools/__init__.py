1ee2af977d4e242d96a12eb3dccc6f5a31e8d100
Benny <83777519+bennyscripts@users.noreply.github.com>
2026-09-10 15:03:49 +0100

    Add backups page from #66 by ItzRealMee with major design changes by Benny

--- gui/pages/tools/__init__.py
+++ gui/pages/tools/__init__.py
@@ -4,3 +4,4 @@ from .message_logger_page import MessageLoggerPage
 from .user_lookup_page import UserLookupPage
 from .password_gen import PasswordGenPage
 from .auto_afk_reply_page import AutoAFKReplyPage
+from .backups_page import BackupsPage
--- gui/pages/tools/tools.py
+++ gui/pages/tools/tools.py
@@ -7,6 +7,7 @@ from gui.pages.tools.message_logger_page import MessageLoggerPage
 from gui.pages.tools.user_lookup_page import UserLookupPage
 from gui.pages.tools.password_gen import PasswordGenPage
 from gui.pages.tools.auto_afk_reply_page import AutoAFKReplyPage
+from gui.pages.tools.backups_page import BackupsPage
 from gui.helpers.style import Style
 
 class ToolsPage:
@@ -23,6 +24,7 @@ class ToolsPage:
         self.user_lookup_page = UserLookupPage(self, root, bot_controller, images, layout)
         self.password_gen_page = PasswordGenPage(self, root, bot_controller, images, layout)
         self.auto_afk_reply_page = AutoAFKReplyPage(self, root, bot_controller, images, layout)
+        self.backups_page = BackupsPage(self, root, bot_controller, images, layout)
         
         self.pages = [
             {
@@ -48,6 +50,12 @@ class ToolsPage:
                 "description": "Automatically reply to DMs when you're away from the keyboard",
                 "page": self.auto_afk_reply_page,
                 "command": self.draw_auto_afk_reply
+            },
+            {
+                "name": "Backups",
+                "description": "Create and restore backups of your Discord account, friends, and servers",
+                "page": self.backups_page,
+                "command": self.draw_backups
             }
             # {
             #     "name": "User Lookup",
@@ -96,6 +104,14 @@ class ToolsPage:
         self.auto_afk_reply_page.draw(main)
         self.layout.sidebar.set_button_command("tools", self.draw_auto_afk_reply)
         self.position_resize_grips()
+        
+    def draw_backups(self):
+        self.layout.sidebar.set_current_page("tools")
+        self.layout.clear()
+        main = self.layout.main()
+        self.backups_page.draw(main)
+        self.layout.sidebar.set_button_command("tools", self.draw_backups)
+        self.position_resize_grips() 
         
     def _bind_hover_effects(self, widget, targets, hover_bg, normal_bg):
         def on_enter(_):
